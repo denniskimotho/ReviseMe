a52f0cadf3c73bcbcd11fb94649011c5f8ab8b72
denniskimotho <denniskimotho222@gmail.com>
2024-07-29 14:03:35 +0300

    Building XMl file for calculator

--- app/src/main/java/com/example/reviseme/MainActivity.kt
+++ app/src/main/java/com/example/reviseme/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.reviseme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val num1: Int = 5
+        var num2: Int
+        num2 = 10
+
+        val sum: Int = num1 + num2
+        val tvDisplay: TextView = findViewById(R.id.tvDisplay)
+        tvDisplay.text = sum.toString()
+
     }
 }
